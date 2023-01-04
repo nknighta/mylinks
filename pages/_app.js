318bf970b6783266fe14fb23b7b14cff82a4d9c5
NknightA <n.knight.pc0627@gmail.com>
2023-01-04 23:05:57 +0900

    edit _app.js file.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import {ChakraProvider} from "@chakra-ui/react";
 import {BasicTheme} from "../components/BasicTheme";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
       <ChakraProvider>
         <BasicTheme>
             <Component {...pageProps} />
+            <Analytics/>
         </BasicTheme>
       </ChakraProvider>
   )
