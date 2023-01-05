fbcd42184ee8de12161cefbca144da553a9553d6
NknightA <n.knight.pc0627@gmail.com>
2023-01-05 19:43:32 +0900

    edit twitter card.

--- components/BasicTheme.js
+++ components/BasicTheme.js
@@ -2,13 +2,25 @@ import { Box,Text } from "@chakra-ui/react";
 import style from "./theme.module.sass"
 import Head from "next/head"
 
-export const BasicTheme = ({children}) => {
+export const BasicTheme = ({children ,title, description, ogImage, url }) => {
+    const pageUrl = "https://nknight-official.vercel.app/"
+    const ogImg = "/favicon.ico";
   return(
       <div>
           <Head>
               <title>Nknight AMAMIYA official</title>
               <link rel="icon" href="/favicon.ico"/>
-              <meta name="twitter:image" content="https://nknight-official.vercel.app/images/favicon.png"/>
+              <meta
+                  name="twitter:description"
+                  content={
+                      description
+                          ? description
+                          : "ページディスクリプション"
+                  }
+              />
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta name="twitter:title" content={title ? title : "ブログタイトル"} />
+              <meta name="twitter:image" content={url ? url : ogImg} />
           </Head>
           <Box className={style.Main}>
               <Box className={style.header}>
