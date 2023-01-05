5a930d0a213c497544f8c50b6f4a54fb35a99040
NknightA <n.knight.pc0627@gmail.com>
2023-01-05 09:30:47 +0900

    edit Index page.

--- components/BasicTheme.js
+++ components/BasicTheme.js
@@ -7,14 +7,7 @@ export const BasicTheme = ({children}) => {
       <div>
           <Head>
               <title>Nknight AMAMIYA official</title>
-              <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png"/>
-                  <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png"/>
-                      <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png"/>
-                          <link rel="manifest" href="../public/site.webmanifest"/>
-                              <link rel="mask-icon" href="../public/safari-pinned-tab.svg" color="#000021"/>
-                                  <meta name="msapplication-TileColor" content="#000021"/>
-                                      <meta name="theme-color" content="#000021"/>
-              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <link rel="icon" href="../public/favicon.ico"/>
           </Head>
           <Box className={style.Main}>
               <Box className={style.header}>
