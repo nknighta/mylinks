3d4979bb373ea4f5e98e0dd921486b3769dfca40
nknighta <n.knight.pc0627@gmail.com>
2023-11-20 12:40:19 +0900

    fix: error ogp image

--- components/BasicTheme.js
+++ components/BasicTheme.js
@@ -6,18 +6,16 @@ export const BasicTheme = ({
     children ,
     pageTitle,
     pageDescription,
-    pageImg,
     pagePath,
     pageImgWidth,
     pageImgHeight,
 }) => {
+    const pageUrl = "https://nknight-official.vercel.app/"
     const defaultTitle = 'nknight amamiya'
     const defaultDescription = 'Web3 development'
-    const defaultImg = '/favicon.ico'
     const title = pageTitle ? `${pageTitle} | ${defaultTitle}` : defaultTitle
     const description = pageDescription ? pageDescription : defaultDescription
     const url = pagePath
-    const imgUrl = pageImg ? pageImg : defaultImg
     const imgWidth = pageImgWidth ? pageImgWidth : 1280
     const imgHeight = pageImgHeight ? pageImgHeight : 640
   return(
@@ -31,10 +29,10 @@ export const BasicTheme = ({
             <meta property="og:site_name" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content={`https://nknight-official.vercel.app/${pageImg}`} />
+            <meta property="og:image" content={`https://nknight-official.vercel.app/header.png`} />
             <meta property="og:image:width" content={String(imgWidth)} />
             <meta property="og:image:height" content={String(imgHeight)} />
-            <meta name="twitter:image" content={`nknight-official.vercel.app/${pageImg}`} />
+            <meta name="twitter:image" content={`https://nknight-official.vercel.app/header.png`} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@ama_dev_1" />
             <link rel="icon" href={"https://nknight-official.vercel.app/favicon.ico"} sizes="any" />
